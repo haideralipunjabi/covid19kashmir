d11ca285d764d88e60b6f24b3b44687a92862bff
Haider Ali Punjabi <haiderali@cyberservices.com>
2020-03-25 03:12:06 +0530

    Custom Data Source

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,13 @@
-const API_URL = "https://api.covid19india.org/raw_data.json"
+const API_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSg-doiJ59mWF5UiJP-tCB6XCqahr9YaXe6eHiyWFyjylHtGRuy5yZrw1ZNWq3etbbyU8Gqz0i5gANp/pub?gid=0&single=true&output=csv"
 function loadData(){
     progressBarVisible(true)
+
     fetch(API_URL).then((response)=>{
-        return response.json()
-    }).then((data)=>{
-        let filteredData = data["raw_data"].filter((item)=>{
-            return item["detectedstate"]==="Jammu and Kashmir"
-        })
-        loadTable(filteredData)
-        loadStats(filteredData);
+        return response.text()
+    }).then((text)=>{
+        let data = ArraysToDict(CSVToArray(text));
+        loadTable(data)
+        loadStats(data);
     });
 }
 
@@ -18,31 +17,32 @@ function loadTable(data){
     for(let patient of data){
         $("#data-table tbody").append(`
         <tr>
-        <td>${patient.patientnumber}</td>
-                      <td>${patient.dateannounced}</td>
-                      <td>${patient.detectedcity}</td>
-                      <td>${patient.detecteddistrict}</td>
-                      <td>${patient.agebracket}</td>
-                      <td>${patient.gender}</td>
-                      <td>${patient.notes}</td>
+        <td>${data.indexOf(patient)+1}</td>
+                      <td>${patient["Date Announced"]}</td>
+                      <td>${patient["City"]}</td>
+                      <td>${patient["District"]}</td>
+                      <td>${patient["Age"]}</td>
+                      <td>${patient["Gender"]}</td>
+                      <td>${patient["Notes"]}</td>
                       <td>${formatSources(patient)}</td>
         </tr>
     `)
     }
 }
 function formatSources(patient){
-    return (`
-        ${patient.source1 ? `<a href="${patient.source1}">1</a>`:""} 
-        ${patient.source2 ? `<a href="${patient.source2}">2</a>`:""} 
-        ${patient.source3 ? `<a href="${patient.source3}">3</a>`:""}
-    `)
+    let links = patient["Sources"].split(",")
+    let sources = []
+    for(link of links){
+         sources.push(`<a href="${link}">${links.indexOf(link)+1}</a>`)
+    } 
+    return sources.join(" ")
 }
 function loadStats(data){
     console.log(data)
     $("#cases_total").html(data.length);
-    $("#cases_active").html(data.filter((item)=>{return item["currentstatus"]==="Hospitalized"}).length)
-    $("#cases_deaths").html(data.filter((item)=>{return item["currentstatus"]==="Deceased"}).length)
-    $("#cases_recovered").html(data.filter((item)=>{return item["currentstatus"]==="Recovered"}).length)
+    $("#cases_active").html(data.filter((item)=>{return item["Status"]==="Hospitalized"}).length)
+    $("#cases_deaths").html(data.filter((item)=>{return item["Status"]==="Deceased"}).length)
+    $("#cases_recovered").html(data.filter((item)=>{return item["Status"]==="Recovered"}).length)
 
 }
 
@@ -114,6 +114,107 @@ function messageVisible(elem){
 //     pagination = limit;
 //     loadTable();
 // }
+
+// ref: http://stackoverflow.com/a/1293163/2343
+    // This will parse a delimited string into an array of
+    // arrays. The default delimiter is the comma, but this
+    // can be overriden in the second argument.    
+
+    function CSVToArray( strData, strDelimiter ){
+        // Check to see if the delimiter is defined. If not,
+        // then default to comma.
+        strDelimiter = (strDelimiter || ",");
+    
+        // Create a regular expression to parse the CSV values.
+        var objPattern = new RegExp(
+            (
+                // Delimiters.
+                "(\\" + strDelimiter + "|\\r?\\n|\\r|^)" +
+    
+                // Quoted fields.
+                "(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
+    
+                // Standard fields.
+                "([^\"\\" + strDelimiter + "\\r\\n]*))"
+            ),
+            "gi"
+            );
+    
+    
+        // Create an array to hold our data. Give the array
+        // a default empty first row.
+        var arrData = [[]];
+    
+        // Create an array to hold our individual pattern
+        // matching groups.
+        var arrMatches = null;
+    
+    
+        // Keep looping over the regular expression matches
+        // until we can no longer find a match.
+        while (arrMatches = objPattern.exec( strData )){
+    
+            // Get the delimiter that was found.
+            var strMatchedDelimiter = arrMatches[ 1 ];
+    
+            // Check to see if the given delimiter has a length
+            // (is not the start of string) and if it matches
+            // field delimiter. If id does not, then we know
+            // that this delimiter is a row delimiter.
+            if (
+                strMatchedDelimiter.length &&
+                strMatchedDelimiter !== strDelimiter
+                ){
+    
+                // Since we have reached a new row of data,
+                // add an empty row to our data array.
+                arrData.push( [] );
+    
+            }
+    
+            var strMatchedValue;
+    
+            // Now that we have our delimiter out of the way,
+            // let's check to see which kind of value we
+            // captured (quoted or unquoted).
+            if (arrMatches[ 2 ]){
+    
+                // We found a quoted value. When we capture
+                // this value, unescape any double quotes.
+                strMatchedValue = arrMatches[ 2 ].replace(
+                    new RegExp( "\"\"", "g" ),
+                    "\""
+                    );
+    
+            } else {
+    
+                // We found a non-quoted value.
+                strMatchedValue = arrMatches[ 3 ];
+    
+            }
+    
+    
+            // Now that we have our value string, let's add
+            // it to the data array.
+            arrData[ arrData.length - 1 ].push( strMatchedValue );
+        }
+    
+        // Return the parsed data.
+        return( arrData );
+    }
+
+function ArraysToDict(data){
+    let return_data = [];
+    for(let i =1; i<data.length;i++){
+        let item = {};
+        for(let col in data[i]){
+            item[data[0][col]] = data[i][col]
+        }
+        return_data.push(item)
+    }
+    return return_data;
+}
 $(document).ready(function(){
     loadData();
 })
+
