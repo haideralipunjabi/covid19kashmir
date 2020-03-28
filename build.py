30ec4c8623cabaf751619616a1be664be3cd0e4a
Haider Ali Punjabi <haiderali@cyberservices.com>
2020-03-28 23:33:26 +0530

    Added sitemap

--- build.py
+++ build.py
@@ -4,12 +4,33 @@ from os.path import isfile, join, splitext
 
 EXCLUDE_FILES = ['navbar.html', 'header.html', 'footer.html', '404.html']
 onlyfiles = [f for f in listdir() if splitext(f)[1]==".html"]
-templateLoader = jinja2.FileSystemLoader(searchpath="./")
-templateEnv = jinja2.Environment(loader=templateLoader)
+def gen_templates():
+    templateLoader = jinja2.FileSystemLoader(searchpath="./")
+    templateEnv = jinja2.Environment(loader=templateLoader)
 
-for f in onlyfiles:
-    if EXCLUDE_FILES.__contains__(f):
-        continue
-    TEMPLATE_FILE = f
-    template = templateEnv.get_template(TEMPLATE_FILE)
-    print(template.render(),file=open(f,"w"))
+    for f in onlyfiles:
+        if EXCLUDE_FILES.__contains__(f):
+            continue
+        TEMPLATE_FILE = f
+        template = templateEnv.get_template(TEMPLATE_FILE)
+        print(template.render(),file=open(f,"w"))
+
+def gen_sitemap():
+    sitemap_file = open("sitemap.xml","w")
+    sitemap_file.write('<?xml version="1.0" encoding="UTF-8"?>')
+    sitemap_file.write('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">')
+    for f in onlyfiles:
+        if EXCLUDE_FILES.__contains__(f):
+            continue
+        sitemap_file.write(
+            '''
+            <url>
+                <loc>%s</loc>
+            </url>
+            '''
+        %("https://covidkashmir.org/"+f.replace("index","").replace(".html","")))
+    sitemap_file.write('</urlset>')
+    sitemap_file.close()
+
+gen_templates()
+gen_sitemap()
