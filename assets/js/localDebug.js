0920a6ff87d6cbaf406d8003bf10c5857eae911d
Mudasir <imudasirali@gmail.com>
2020-03-29 12:21:34 +0530

    Update localDebug.js

--- assets/js/localDebug.js
+++ assets/js/localDebug.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    if(!window.location.href.includes("covidkashmir.netlify.com") && !window.location.href.includes("netlify.com")){
+    if(!window.location.href.includes("covidkashmir.netlify.com")){
         $("#include-header").load("/header.html")
         $("#include-navbar").load("/navbar.html")
         $("#include-footer").load("/footer.html")
