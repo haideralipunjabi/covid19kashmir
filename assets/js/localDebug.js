67889826bba70aa72249ba6d87c40067e290adf0
Mudasir <imudasirali@gmail.com>
2020-03-29 12:21:59 +0530

    Update localDebug.js

--- assets/js/localDebug.js
+++ assets/js/localDebug.js
@@ -1,8 +1,5 @@
 $(document).ready(function(){
-    if(!window.location.href.includes("covidkashmir.netlify.com")){
         $("#include-header").load("/header.html")
         $("#include-navbar").load("/navbar.html")
         $("#include-footer").load("/footer.html")
-
-    }
 })
