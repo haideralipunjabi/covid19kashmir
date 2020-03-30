80ed18e00318590211887aca1c2899e545fc1191
Haider Ali Punjabi <haiderali@cyberservices.com>
2020-03-30 18:24:50 +0530

    Testing PWA

--- build.py
+++ build.py
@@ -40,7 +40,7 @@ def gen_favicons():
     manifest = json.load(open("manifest.json","r"))
     icons = manifest["icons"]
     for icon in icons:
-        svg2png(url=icon_svg, write_to=icon["src"][1:], parent_width=int(icon["sizes"].split("x")[0]), parent_height=(icon["sizes"].split("x")[1]))
+        svg2png(url=icon_svg, write_to=icon["src"][1:], parent_width=int(icon["sizes"].split("x")[0]), parent_height=int(icon["sizes"].split("x")[1]))
     Image.open("assets/favicons/icon-512x512.png").save("assets/favicons/favicon.ico")
     template = templateEnv.get_template("favicons.html")
     print(template.render(icons=icons,appname=manifest["short_name"], appcolor=manifest["theme_color"], bgcolor=manifest["background_color"]),file=open("favicons.html","w"))
