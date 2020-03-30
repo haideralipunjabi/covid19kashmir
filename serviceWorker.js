c35e04cf11c316d095660fab61804d099cbe80d2
Haider Ali Punjabi <haiderali@cyberservices.com>
2020-03-30 15:37:14 +0530

    Testing PWA

--- serviceWorker.js
+++ serviceWorker.js
@@ -58,6 +58,7 @@ self.addEventListener('install', function(event) {
   );
 });
 self.addEventListener('fetch', function(event) {
+  console.log(event)
   event.respondWith(
     caches.match(event.request)
       .then(function(response) {
