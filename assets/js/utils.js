234b00027769e08dd90b216ea82c884f04a1aee1
Haider Ali Punjabi <haiderali@cyberservices.com>
2020-03-30 13:36:56 +0530

    Fix navabr

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -185,3 +185,11 @@ Object.defineProperties(Array.prototype, {
         }
     }
 });
+$(document).ready(function () {
+    $(".navbar-burger").click(function () {
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      $(".navbar-burger").toggleClass("is-active");
+      $(".navbar-menu").toggleClass("is-active");
+
+    });
+  })
