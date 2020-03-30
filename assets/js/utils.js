920a9e7043fef0d7ba6a8e6438d295063846eeed
Haider Ali Punjabi <haiderali@cyberservices.com>
2020-03-30 15:11:12 +0530

    Registered SW

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -193,3 +193,14 @@ $(document).ready(function () {
 
     });
   })
+
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/serviceWorker.js').then(function(registration) {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
