5dbde8d755370e4769c86486c3742f0dcebe32cf
Haider Ali Punjabi <haiderali@cyberservices.com>
2020-03-31 10:12:36 +0530

    Moving to Travis

--- assets/js/localDebug.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-        if(!window.location.href.includes("covidkashmir.org") && !window.location.href.includes("netlify.com")){
-        $("#include-header").load("/header.html")
-        $("#include-navbar").load("/navbar.html")
-        $("#include-footer").load("/footer.html")
-
-    }
-})
