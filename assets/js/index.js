5a05d2abfca0d355e549bbcaca0856b4c3215b79
Haider Ali Punjabi <haiderali@cyberservices.com>
2020-04-07 17:30:16 +0530

    Fixed stats

--- assets/js/index.js
+++ assets/js/index.js
@@ -133,7 +133,7 @@ function loadStats(data) {
     $("#cases_deaths").html(data.Deceased);
     $("#cases_recovered").html(data.Recovered);
     $("#cases_total_today").html(data.Total - data.TotalYesterday);
-    $("#cases_active_today").html(data.Active - data.ActiveYesterday);
+    $("#cases_active_today").html((data.Active - data.ActiveYesterday) + (data.Deceased - data.DeceasedYesterday) + (data.Recovered - data.RecoveredYesterday));
     $("#cases_deaths_today").html(data.Deceased - data.DeceasedYesterday);
     $("#cases_recovered_today").html(data.Recovered - data.RecoveredYesterday);
 
