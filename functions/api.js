344c97a55e17a90e823ea7c83c9189865e31a9e2
Haider Ali Punjabi <haiderali@cyberservices.com>
2020-04-10 19:37:04 +0530

    Fixed districts

--- functions/api.js
+++ functions/api.js
@@ -3,7 +3,7 @@ const fetch = require("node-fetch");
 const Utils = require("./utils")
 const Stats = require("./stats")
 const { URL_BULLETIN, URL_PATIENTS } = process.env;
-
+let URL_DISTRICTS = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSg-doiJ59mWF5UiJP-tCB6XCqahr9YaXe6eHiyWFyjylHtGRuy5yZrw1ZNWq3etbbyU8Gqz0i5gANp/pub?gid=1857127229&single=true&output=csv"
 exports.handler = async (event, context) => {
   let fields = event.queryStringParameters.fields;
   if(!fields){
@@ -20,6 +20,9 @@ exports.handler = async (event, context) => {
   if(fields.includes("variance")){
     promises.push(fetch(URL_BULLETIN).then(response=>response.text()))
   }
+  if(fields.includes("districtMap")){
+    promises.push(fetch(URL_DISTRICTS).then(response=>response.text()))
+  }
   return Promise.all(promises).then(values=>{
     patientData = Utils.ArraysToDict(Utils.CSVToArray(values[0]))
     data = {}
@@ -27,7 +30,9 @@ exports.handler = async (event, context) => {
       data["patientData"] = Utils.ArraysToDict(Utils.CSVToArray(values[0]))
     }
     if(fields.includes("districtMap")){
-      data["districtMap"] = Stats.DistrictMap(patientData)
+      let index = (fields.includes("variance")) ? 1 : 0 
+      districtData = Utils.ArraysToDict(Utils.CSVToArray(values[1+index]))
+      data["districtMap"] = Stats.DistrictMap(districtData)
     }
     if(fields.includes("dailyMap")){
       data["dailyMap"] = Stats.DailyMap(patientData)
