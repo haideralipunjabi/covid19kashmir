de8fdfeb4e7e89d5bdd29188058727795d888848
Haider Ali Punjabi <haiderali@cyberservices.com>
2020-04-11 18:15:09 +0530

    Shifting to workbox

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-workbox.google-analytics.initialize()
+workbox.googleAnalytics.initialize()
 workbox.routing.registerRoute(
   /assets/,
   new workbox.strategies.StaleWhileRevalidate({
