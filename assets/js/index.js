00f630f6b8ee276cf472f54c5cb52de8e4348e42
Haider Ali Punjabi <haiderali@cyberservices.com>
2020-04-11 09:40:09 +0530

    Moved District Table

--- assets/js/index.js
+++ assets/js/index.js
@@ -72,11 +72,11 @@ function loadDistricts(){
         if(dis[0]==="Unknown") continue;
         $("#district-table tbody").append(`
             <tr>
-                <td>${dis[0]}</td>
-                <td>${dis[1]["Total"]}</td>
-                <td>${dis[1]["Active"]}</td>
-                <td>${dis[1]["Recovered"]}</td>
-                <td>${dis[1]["Deceased"]}</td>
+                <td class="has-text-centered">${dis[0]}</td>
+                <td class="has-text-centered">${dis[1]["Total"]}</td>
+                <td class="has-text-centered">${dis[1]["Active"]}</td>
+                <td class="has-text-centered">${dis[1]["Recovered"]}</td>
+                <td class="has-text-centered">${dis[1]["Deceased"]}</td>
             </tr>
         `)
     }
@@ -151,18 +151,6 @@ function loadStats(data) {
 
 function loadNews(data) {
     let $container = $("#news-container")
-    // let columns = data.map(item => $(`<div class="column">${$(item["html"]).attr("data-width","220")[0].outerHTML}</div>`))
-    // for (let column of columns) {
-    //     for (let item of column) {
-    //         twttr.widgets.load(item)
-    //     }
-    // }
-    // for (let column of columns) {
-    //     for (let item of column) {
-
-    //         $container.append(item)
-    //     }
-    // }
     for(let item of data){
         let div = $("<div class='column'></div>")[0]
         twttr.widgets.createTweet(item["url"].split("/").slice(-1)[0], div, {
