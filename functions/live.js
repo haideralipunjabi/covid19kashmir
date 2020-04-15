e17a20d64f7d6ea87bd3b1e842e3993ab0836106
Haider Ali Punjabi <haiderali@cyberservices.com>
2020-04-15 11:08:06 +0530

    Moving to gist

---
+++ functions/live.js
@@ -0,0 +1,10 @@
+const fetch = require("node-fetch")
+const LIVE_URL = "https://gist.githubusercontent.com/haideralipunjabi/3743624e604ed3a81a40ca2ec44d7c9c/raw/"
+exports.handler = async (event,context)=>{
+    return fetch(LIVE_URL+"?v="+Math.floor(Math.random()*10**10).toString()).then(r=>r.json()).then(data=>{
+        return {
+            statusCode: 200,
+            body:JSON.stringify(data)
+          }
+    })
+}
