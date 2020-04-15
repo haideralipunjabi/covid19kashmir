4eecf216ec987eea977523785c5c01751a87cad4
Haider Ali Punjabi <haiderali@cyberservices.com>
2020-04-15 11:37:58 +0530

    Fixed CORS

--- functions/live.js
+++ functions/live.js
@@ -4,6 +4,9 @@ exports.handler = async (event,context)=>{
     return fetch(LIVE_URL+"?v="+Math.floor(Math.random()*10**10).toString()).then(r=>r.json()).then(data=>{
         return {
             statusCode: 200,
+            headers:{
+                'Access-Control-Allow-Origin': '*',
+            },
             body:JSON.stringify(data)
           }
     })
