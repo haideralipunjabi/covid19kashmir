c4e6bfadfc4b7d775d1649d5ed66fbb90893f326
Haider Ali Punjabi <haiderali@cyberservices.com>
2020-05-09 11:47:11 +0530

    FIxed Sources

--- assets/js/index.js
+++ assets/js/index.js
@@ -186,11 +186,11 @@ function formatSources(patient) {
     let links = patient["Sources"].split(",")
     let sources = []
     for (link of links) {
-        if(!link.startsWith("http")){
-            sources.push(`<a href=https://"${link}">${links.indexOf(link)+1}</a>`)
+        if(!link.trim().startsWith("http")){
+            sources.push(`<a href="https://${link.trim()}">${links.indexOf(link)+1}</a>`)
             continue;
         }
-        sources.push(`<a href="${link}">${links.indexOf(link)+1}</a>`)
+        sources.push(`<a href="${link.trim()}">${links.indexOf(link)+1}</a>`)
     }
     return sources.join(" ")
 }
